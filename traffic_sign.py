0872d691bc7fc914ae4c0fb6815fec71e88519d1
Dibyanshu Patnaik <dibyanshupatnaik@gmail.com>
2020-04-18 13:21:57 +0530

    initial

---
+++ traffic_sign.py
@@ -0,0 +1,137 @@
+import numpy as np 
+import pandas as pd 
+import matplotlib.pyplot as plt
+import cv2
+import tensorflow as tf
+from PIL import Image
+import os
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+from keras.models import Sequential, load_model
+from keras.layers import Conv2D, MaxPool2D, Dense, Flatten, Dropout
+
+data = []
+labels = []
+classes = 43
+cur_path = os.getcwd()
+print(cur_path)
+
+# Retrieving the images and their labels
+for i in range(classes):
+    path = os.path.join(cur_path,'train',str(i))
+    images = os.listdir(path)
+
+
+    for a in images:
+        try:
+            #print(a)
+            #print(path)
+            image = Image.open(path + '/'+ a)
+            image = image.resize((30,30))
+            image = np.array(image)
+            #sim = Image.fromarray(image)
+            data.append(image)
+            labels.append(i)
+        except:
+            print(a)
+            print(path)
+            print("Error loading image")
+
+#Converting lists into numpy arrays
+data = np.array(data)
+labels = np.array(labels)
+
+print(data.shape, labels.shape)
+#Splitting training and testing dataset
+X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=42)
+
+print(X_train.shape, X_test.shape, y_train.shape, y_test.shape)
+
+#Converting the labels into one hot encoding
+y_train = to_categorical(y_train, 43)
+y_test = to_categorical(y_test, 43)
+
+#Building the model
+model = Sequential()
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='selu', input_shape=X_train.shape[1:]))
+model.add(Conv2D(filters=32, kernel_size=(5,5), activation='selu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='selu'))
+model.add(Conv2D(filters=64, kernel_size=(3, 3), activation='selu'))
+model.add(MaxPool2D(pool_size=(2, 2)))
+model.add(Dropout(rate=0.25))
+model.add(Flatten())
+model.add(Dense(256, activation='selu'))
+model.add(Dropout(rate=0.5))
+model.add(Dense(43, activation='softmax'))
+
+
+
+#Compilation of the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+epochs = 15
+history = model.fit(X_train, y_train, batch_size=64, epochs=epochs, validation_data=(X_test, y_test))
+model.save("my_model.h5")
+
+#plotting graphs for accuracy 
+plt.figure(0)
+plt.plot(history.history['accuracy'], label='training accuracy')
+plt.plot(history.history['val_accuracy'], label='val accuracy')
+plt.title('Accuracy')
+plt.xlabel('epochs')
+plt.ylabel('accuracy')
+plt.legend()
+plt.show()
+
+plt.figure(1)
+plt.plot(history.history['loss'], label='training loss')
+plt.plot(history.history['val_loss'], label='val loss')
+plt.title('Loss')
+plt.xlabel('epochs')
+plt.ylabel('loss')
+plt.legend()
+plt.show()
+
+#testing accuracy on test dataset
+from sklearn.metrics import accuracy_score
+
+y_test = pd.read_csv('Test.csv')
+
+labels = y_test["ClassId"].values
+imgs = y_test["Path"].values
+
+data=[]
+
+for img in imgs:
+    image = Image.open(img)
+    image = image.resize((30,30))
+    data.append(np.array(image))
+
+X_test=np.array(data)
+
+pred = model.predict_classes(X_test)
+
+#Accuracy with the test data
+from sklearn.metrics import accuracy_score
+print(accuracy_score(labels, pred))
+
+'''
+The architecture of our model is:
+
+2 Conv2D layer (filter=32, kernel_size=(5,5), activation=”relu”)
+MaxPool2D layer ( pool_size=(2,2))
+Dropout layer (rate=0.25)
+2 Conv2D layer (filter=64, kernel_size=(3,3), activation=”relu”)
+MaxPool2D layer ( pool_size=(2,2))
+Dropout layer (rate=0.25)
+Flatten layer to squeeze the layers into 1 dimension
+Dense Fully connected layer (256 nodes, activation=”relu”)
+Dropout layer (rate=0.5)
+Dense layer (43 nodes, activation=”softmax”)
+
+'''
+
+
+
